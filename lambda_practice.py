b477885be498abf6ad6b3fc0033eec261dcbd857
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2021-06-29 16:32:37 +0800

    Update lambda_practice.py

--- lambda_practice.py
+++ lambda_practice.py
@@ -1,15 +1,20 @@
 # lambda function
 numbers = [23, 55, 6, 97, 49]
+
 #filter
 print(list(filter(lambda x:x>50,numbers)))
+# >>> [55, 97]
 
 #map
 print(list(map(lambda x:x*2,numbers)))
+# >>> [46, 110, 12, 194, 98]
 
 #reduce
 from functools import reduce
 print(reduce(lambda x, y:x+y,numbers))
+# >>> 230
 
 #sorted
 pokemons = [("Eevee", 55), ("pikachu", 36)]
 print(sorted(pokemons, key = lambda pokemons: pokemons[1]))
+# >>> [('pikachu', 36), ('Eevee', 55)]
