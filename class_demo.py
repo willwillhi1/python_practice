132930097d60082c1b4a24e12a19774fd0a6f366
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2021-06-29 15:56:11 +0800

    Update class_demo.py

--- class_demo.py
+++ class_demo.py
@@ -10,7 +10,35 @@ class Pokemon:
     
     def showMove(self):
         print(self.movelist)
+    
+    def fight(self, opponent):
+        if self.Atk > opponent.Atk:
+            print("win!")
+        elif self.Atk < opponent.Atk:
+            print("lose!")
+        else:
+            print("draw!")
+            
+            
+#Inheritance
+class Pokemon1(Pokemon):
+    #pass //same as Pokemon
+    #override
+    def __init__(self, Atk, Hp, Def):
+        self.Atk = Atk
+        self.Hp = Hp
+        self.Def = Def
+    def fight(self, opponent):
+        super().fight(self, opponent)
+        print('!!')
+    
         
 Pikachu = Pokemon(55, 36)
 Pikachu.addMove("Nuzzle")
 Pikachu.showMove()
+Eevee = Pokemon(55, 55)
+Pikachu.fight(Eevee)
+# >>> draw
+
+
+
